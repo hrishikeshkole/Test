2fb7d036db16d089e3e9834bcbcfba93033f3762
Hrishikesh Kole <hrishikole230801@gmail.com>
2026-04-17 14:44:48 +0530

    Add greet and add functions with corrected syntax
    Fix syntax errors in greet, for loop, if statement, and add function.

---
+++ test.py
@@ -0,0 +1,13 @@
+def greet(name)
+    print("Hello, " + name)
+
+for i in range(5)
+    print(i)
+
+if True
+    print("This is true")
+else:
+    print("This is false"
+
+def add(a, b):
+return a + b
